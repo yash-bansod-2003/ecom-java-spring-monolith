f3e86cd41d7b3d0e78af4c13eed25e188e01a221
Yash Bansod <yashbansodgit@gmail.com>
2026-02-10 19:34:21 +0530

    feat: introduce the service with Jpa Repository

---
+++ src/main/java/com/example/learn/UserService.java
@@ -0,0 +1,37 @@
+package com.example.learn;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return  this.userRepository.findAll();
+    }
+
+    public void addUser(User user) {
+        this.userRepository.save(user);
+    }
+
+    public Optional<User> findById(Long id) {
+        return this.userRepository.findById(id);
+    }
+
+    public boolean updateUser(Long id, User user) {
+        return this.userRepository.findById(id)
+                .map(existingUser -> {
+                    existingUser.setName(user.getName());
+                    existingUser.setEmail(user.getEmail());
+                    this.userRepository.save(existingUser);
+                    return true;
+                }).orElse(false);
+    }
+}
